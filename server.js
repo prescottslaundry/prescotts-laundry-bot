725735eedcaa25c9bef32cb2d4a364de886ca6d9
prescottslaundry <prescottslaundry@gmail.com>
2026-01-20 16:41:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ import twilio from "twilio";
 import { SYSTEM_PROMPT } from "./prompt.js";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log("REQ:", req.method, req.url);
+  next();
+});
 
 const {
   OPENAI_API_KEY,
@@ -135,6 +144,7 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
 
 
 
